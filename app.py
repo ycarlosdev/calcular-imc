d73c8a4bc2e9ecd5ff1e43488a109a44727fea01
Yan Carlos <ycarlosdev@gmail.com>
2026-09-03 12:20:34 -0400

    agreando interactividad con el servidor

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask,render_template,request
+from formularios import DatosImc
+import uuid
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = str(uuid.uuid4())
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/",methods=["GET","POST"])
+def welcome():
+    form = DatosImc()
+    if form.validate_on_submit():
+        peso = form.peso.data
+        talla = form.talla.data
+        return render_template("index.html",form=form)
+    return render_template("index.html",form=form)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
